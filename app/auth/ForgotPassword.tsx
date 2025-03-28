9b4ffea26e8ff7576e1e36143f5b450906b379df
Pinto Manuel <pintoydc@gmail.com>
2025-03-28 14:03:25 +0200

    Commit code

--- app/auth/ForgotPassword.tsx
+++ app/auth/ForgotPassword.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable } from 'react-native';
+import { View, Text, TextInput, Pressable, Image } from 'react-native';
 import { useRouter } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
-import styles from './signup.styles'; 
+import styles from './signup.styles';
+
 export default function ForgotPasswordScreen() {
   const router = useRouter();
   const [email, setEmail] = useState('');
@@ -14,7 +15,9 @@ export default function ForgotPasswordScreen() {
 
   return (
     <LinearGradient
-      colors={['#3B5998', '#4FBDBA']}
+      colors={['#1A237E', '#4FBDBA']}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 0.8 }}
       style={styles.gradient}
     >
       <View style={styles.container}>
@@ -31,6 +34,10 @@ export default function ForgotPasswordScreen() {
           <Text style={styles.signUpButtonText}>Reset Password</Text>
         </Pressable>
       </View>
+      <Image
+        source={require('../../assets/images/pos.png')}
+        style={[styles.bottomImage, { width: 500, height: 420, marginTop: 105 }]}
+      />
     </LinearGradient>
   );
 }
